efffabac6f0b164117d8a13454e1ce668f3930c2
Maria Eduarda Caixeta <mariaeduarda.caixeta@performait.com>
2026-02-11 18:59:40 -0300

    adding fonts and linear gradient

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,14 +1,10 @@
-import { router } from "expo-router";
-import { Button, Text, View } from "react-native";
+import { HomeHeader } from "@/components/HomeHeader";
+import { View } from "react-native";
 
 export default function Index() {
     return (
-        <View style={{ flex: 1, justifyContent: "center" }}>
-            <Text>Olá, Expo Router</Text>
-
-            <Button title="Target" onPress={() => router.navigate('target')} />
-            <Button title="Transaction" onPress={() => router.navigate('/transaction/132')} />
-            <Button title="Progresso" onPress={() => router.navigate('/in-progress/132')} />
+        <View style={{ flex: 1 }}>
+            <HomeHeader />
         </View>
     )
 }
