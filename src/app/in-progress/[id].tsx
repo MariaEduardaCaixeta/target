a46cb70be8f0a8fc0a23cdb89f3c2f105249fddc
Maria Eduarda Caixeta <mariaeduarda.caixeta@performait.com>
2026-02-12 15:14:23 -0300

    progress bar component

--- src/app/in-progress/[id].tsx
+++ src/app/in-progress/[id].tsx
@@ -1,13 +1,31 @@
-import { router, useLocalSearchParams } from "expo-router";
-import { Button, Text, View } from "react-native";
+import { PageHeader } from "@/components/PageHeader";
+import { Progress } from "@/components/Progress";
+import { useLocalSearchParams } from "expo-router";
+import { View } from "react-native";
+
+const details = {
+    current: "R$ 1.500,00",
+    target: "R$ 3.000,00",
+    percentage: 50,
+}
 
 export default function InProgress() {
-    const params = useLocalSearchParams<{id: string}>()
+    const params = useLocalSearchParams<{ id: string }>()
 
     return (
-        <View style={{ flex:1, justifyContent:"center", alignItems:"center" }}>
-            <Text>id: {params.id}</Text>
-            <Button title="Voltar" onPress={()=>router.back()}/>
+        <View style={{ flex: 1, padding: 24, gap: 32, }}>
+            <PageHeader
+                title="Apple Watch"
+                rightButton={{
+                    icon: 'edit',
+                    onPress: () => { }
+                }}
+            />
+
+            <Progress
+                data={details}
+            />
+
         </View>
     )
 }
