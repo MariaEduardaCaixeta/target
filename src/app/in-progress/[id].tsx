2495c40010f412522a295bb7356650355fc1cffc
Maria Eduarda Caixeta <mariaeduarda.caixeta@performait.com>
2026-02-12 15:33:02 -0300

    creating transaction component

--- src/app/in-progress/[id].tsx
+++ src/app/in-progress/[id].tsx
@@ -2,12 +2,32 @@ import { PageHeader } from "@/components/PageHeader";
 import { Progress } from "@/components/Progress";
 import { useLocalSearchParams } from "expo-router";
 import { View } from "react-native";
+import { List } from "@/components/List";
+import { Transaction, TransactionProps } from "@/components/Transaction";
+import { TransactionTypes } from "@/utils/TransactionTypes";
 
 const details = {
     current: "R$ 1.500,00",
     target: "R$ 3.000,00",
     percentage: 50,
 }
+
+const transactions: TransactionProps[] = [
+    {
+        id: "1",
+        value: "R$ 500,00",
+        date: "01/01/2024",
+        description: "Salário",
+        type: TransactionTypes.INCOME
+    },
+    {
+        id: "2",
+        value: "R$ 200,00",
+        date: "02/01/2024",
+        description: "Aluguel",
+        type: TransactionTypes.EXPENSE
+    },
+]
 
 export default function InProgress() {
     const params = useLocalSearchParams<{ id: string }>()
@@ -24,6 +44,17 @@ export default function InProgress() {
 
             <Progress
                 data={details}
+            />
+
+            <List
+                title="Transações"
+                data={transactions}
+                renderItem={({ item }) => (
+                    <Transaction
+                        data={item}
+                        onRemove={() => { }}
+                    />
+                )}
             />
 
         </View>
