687a8752292e5f28a254fa8049d518202fd7019f
Maria Eduarda Caixeta <mariaeduarda.caixeta@performait.com>
2026-02-12 14:50:14 -0300

    input component

--- src/app/target.tsx
+++ src/app/target.tsx
@@ -1,13 +1,18 @@
+import { Button } from "@/components/Button";
+import { Input } from "@/components/Input";
 import { PageHeader } from "@/components/PageHeader";
 import { router } from "expo-router";
-import { Button, Text, View } from "react-native";
+import { Text, View } from "react-native";
 
 export default function Target() {
     return (
         <View style={{ flex: 1, padding: 24 }}> 
             <PageHeader title="Meta" subtitle="Economize para..."/>
 
-            <Button title="Voltar" onPress={() => router.back()}/>
+            <View style={{ marginTop: 32, gap: 24 }}>
+                <Input label="Nome da meta" placeholder="Ex: Viagem para praia, Apple Watch..."/>
+                <Button title="Salvar"/>
+            </View>
         </View>
     )
 }
